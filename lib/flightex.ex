bd832069c3b316281153d8b0c69ef1eec93bca70
Wilton Souza <willsouzafilho@gmail.com>
2022-03-03 16:22:02 -0300

    Finishing challenge 02 implementation

--- lib/flightex.ex
+++ lib/flightex.ex
@@ -1,2 +1,14 @@
 defmodule Flightex do
+  alias Flightex.Bookings.Agent, as: BookingsAgent
+  alias Flightex.Bookings.CreateOrUpdate, as: CreateOrUpdateBookings
+  alias Flightex.Users.Agent, as: UsersAgent
+  alias Flightex.Users.CreateOrUpdate, as: CreateOrUpdateUser
+
+  def start_agents do
+    UsersAgent.start_link(%{})
+    BookingsAgent.start_link(%{})
+  end
+
+  defdelegate create_or_update_user(params), to: CreateOrUpdateUser, as: :call
+  defdelegate create_or_update_booking(params), to: CreateOrUpdateBookings, as: :call
 end
